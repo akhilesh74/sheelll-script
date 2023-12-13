e9fe827b652d59001fd0d9bc0f2dba7cb9afae7c
akhilesh74 <akhilehakhi1993@gmail.com>
2023-12-13 22:12:43 +0530

    for practice

---
+++ 05-function.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+VALIDATE (){
+    if [$? -ne 0]
+    then 
+        echo " mysql  installation is failed"
+        exit 1
+    else 
+        echo "mysql is installed successfully"
+    fi
+
+}
+if [ $ID -ne 0 ]
+then 
+    echo "Error: please use root "
+else
+    echo "yau are in root"
+fi
+
+yum install mysql -y 
+
+VALIDATE
+
+yum install git -y
+
+VALIDATE
