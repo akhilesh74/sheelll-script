e5fe9df8a191251b2c3a3ad078d562b3dfd5200b
akhilesh74 <akhilehakhi1993@gmail.com>
2023-12-20 10:57:08 +0530

    added the script

--- install-package.sh
+++ install-package.sh
@@ -1,10 +1,14 @@
 #!bin/bash
 
 ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 if [ $ID -ne 0 ]
 then 
-    echo "Error: please use root "
+    echo -e "$R Error: please use root $N"
 else
     echo "yau are in root"
 fi
