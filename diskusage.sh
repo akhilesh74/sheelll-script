b183777c2ac6beb84fec82ea9466b34664330fb9
akhilesh74 <akhilehakhi1993@gmail.com>
2023-12-21 08:47:06 +0530

    added the script

---
+++ diskusage.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DIST_USAGE=$(df -hT | grep -vE 'tmp|File')
+DISK_THRESHOLD=1
+message=""
+
+while IFS= read line
+do
+    usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
+    partition=$(echo $line |awk '{print $1F}' )
+    if [$usage -ge $DISK_THRESHOLD]
+    then 
+        message+="High disk usage on $partition : $usage \n"
+    fi
+done <<< $DIST_USAGE
+
+echo -e "message : $message"
